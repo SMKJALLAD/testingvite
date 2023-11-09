9f2f6b7ce6f2d930e598d65b233f51fb3a32d720
DuplicateRabbit <35868327+SMKJALLAD@users.noreply.github.com>
2023-11-09 15:30:28 +0400

    please god

---
+++ assets/entries/pages_faqs_index.page.772c2c86.js
@@ -0,0 +1,59 @@
+import{r as i,j as e}from"../chunks/chunk-8798a010.js";import{s as a,N as c,L as h}from"../chunks/chunk-fa60bfb8.js";import{_ as g,j as p,$ as m,V as u}from"../chunks/chunk-7adb0cef.js";import"../chunks/chunk-3b04216b.js";const y=a.div`
+
+  height: 100vh;
+  scroll-behavior: smooth;
+  scroll-snap-type: none;
+  overflow-y: auto;
+  scrollbar-width: none;
+  scroll-snap-align: center;
+  background-image: url('/img/bgquestionCompressedDarkened.jpg'); // Add your image path here
+  background-size: cover; // Ensure the image covers the entire section
+  background-position: center; // Center the image
+`,b=a(g)`
+  --mdb-accordion-border-color: #e0e0e000;
+`,w=a(p)`
+  .accordion-button {
+    background-color: #fff0;
+    color: white;
+    font-size: 20px;
+  }
+  color: white;
+  background: #fff0;
+  .accordion-body {
+    font-size: 18px; // Set the desired font size for the body text here
+  }
+  .accordion-button {
+    font-size: 18px; // Adjust as needed
+  }
+  .accordion-body {
+    font-size: 16px; // Adjust as needed
+  }
+`,f=a.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+  padding-bottom: 1000px;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    gap: 20px;
+    padding-bottom: 50px; // Adjust padding as needed
+  }
+`,v=a.div`
+  font-size: 22px;
+  color:white;
+  @media only screen and (max-width: 768px) {
+    font-size: 18px;
+  }
+`,x=()=>{const[o,t]=i.useState(!1),n=[{collapseId:1,headerTitle:"What Differentiates Skyvertise's Drone Shows and How Are They Executed?",body:"Skyvertise merges advanced drone technology and AI, creating uniquely mesmerizing drone shows and aerial advertisements. We're capable of crafting custom shapes, logos, messages, and synchronizing shows with music and other multimedia, providing a highly customizable and innovative advertising platform. Our shows also stand out for their safety, sustainability, and the ability to operate multiple distinct displays in one day."},{collapseId:2,headerTitle:"How is Safety Ensured in Skyvertise Drone Shows?",body:"Comprehensive pre-show planning, risk assessments, real-time monitoring, and a skilled team of engineers and operators ensure the safety of our drone light shows. We also adhere to regulatory guidelines and create exclusion zones during shows for additional safety."},{collapseId:3,headerTitle:"What is the Booking and Preparation Process for a Skyvertise Drone Show or Advertisement?",body:"To book, reach out via website, email, or phone, and our team will guide you through consultation, planning, and design phases. We recommend booking several weeks to months in advance to allow for thorough preparation, which includes designing, rehearsal, and obtaining necessary permissions and approvals."},{collapseId:4,headerTitle:"Is Skyvertise’s Approach Environmentally Friendly?",body:"Yes, our drone shows offer an eco-friendly alternative to traditional fireworks and don’t produce noise pollution or waste, aligning with global sustainability goals."},{collapseId:5,headerTitle:"How Does Skyvertise Handle Regulatory and Legal Aspects?",body:"Skyvertise manages all legal aspects, including securing the necessary permits and regulatory approvals, ensuring all shows comply with legal and safety standards worldwide."},{collapseId:6,headerTitle:"Where Can Skyvertise’s Services Be Utilized and Do They Offer International Services?",body:"Headquartered in Dubai, UAE, Skyvertise offers drone light show services globally, managing international events and adapting to various regulatory and environmental conditions to deliver safe aerial displays."},{collapseId:7,headerTitle:"How Customizable and Interactable Are Drone Shows?",body:"Extremely. Drones act as light pixels that can be controlled to create intricate designs, animations, and sequences. Shows can also be integrated with music, laser lights, and other multimedia to create immersive experiences."},{collapseId:8,headerTitle:"How Does Skyvertise Enhance Branding and Contribute to Tourism?",body:"Skyvertise’s drone shows create emotional connections and memorable experiences, ensuring your brand message is retained long after the show. Additionally, the visual spectacles serve as attractions, potentially enhancing local tourism and business."},{collapseId:9,headerTitle:"How Can I Stay Updated and Explore Past Work of Skyvertise?",body:"You can stay informed about the latest news and offerings by subscribing to our newsletter and visit our '''Portfolio''' section on our website to view previous drone advertising campaigns."},{collapseId:10,headerTitle:"Can Drone Light Shows Be Affected by Weather and Are They Suitable Indoors?",body:"Weather conditions like rain or high winds may impact shows, which may be rescheduled or adapted accordingly. And yes, with adequate safety measures and space, drone shows can be executed indoors."}],r=[{collapseId:11,headerTitle:"what's the world record for most drones used in a single drone show?",body:"3,051 drones. This record was achieved by EFYI Group and Shenzhen Damoda Intelligent Control Technology Co., Ltd. in Zhuhai, Guangdong, China, on 20 September 2020."},{collapseId:12,headerTitle:"How many drones are used in a typical show?",body:`Small Shows: 20 to 100 drones
+
+Medium Shows: 100 to 500 drones
+
+Large Shows: 500 to 2,000 drones
+
+Mega Shows: 2,000+ drones`},{collapseId:13,headerTitle:"What's the average duration of a drone light show?",body:`Short Shows: 5-10 minutes
+
+Standard Shows: 10-15 minutes
+
+Extended Shows: 15-30 minutes`},{collapseId:14,headerTitle:"What visual appeal do drone light shows provide?",body:"Drone light shows offer a unique and captivating visual experience that can stand out remarkably from traditional advertising methods. Their novel and mesmerizing nature can create a significant and lasting impression on audiences."},{collapseId:15,headerTitle:"Can drone light shows be customized for branding?",body:"Yes, drones can be programmed to create specific shapes, patterns, logos, or even animated sequences, enabling brands to display their logos or related imagery in a grand, 3D aerial format."},{collapseId:16,headerTitle:"How do drone light shows have viral potential?",body:"The awe-inspiring visuals from drone light shows often get shared on social media platforms, leading to organic reach and potential viral attention, which can significantly boost the visibility of the displayed content or brand."},{collapseId:17,headerTitle:"How do drone light shows attract media coverage?",body:"Due to their innovative and spectacular nature, drone light shows often draw media coverage, amplifying the reach of the marketing or branding event and consequently, increasing the brand's visibility to a wider audience."},{collapseId:18,headerTitle:"How are drone light shows sustainable?",body:"Compared to traditional fireworks, drone light shows are environmentally friendly, thus representing and promoting sustainability. This eco-friendly aspect can enhance a brand's image, especially among environmentally-conscious consumers."},{collapseId:19,headerTitle:"In what types of events can drone light shows be incorporated?",body:"Drone light shows are versatile and can be incorporated into various events, including product launches, festivals, sports events, and corporate celebrations, providing an adaptable solution for different marketing and celebration contexts."},{collapseId:20,headerTitle:"How safe are drone light shows compared to traditional fireworks?",body:"Drone shows, not involving risks such as fire or noise disturbances associated with fireworks, can be considered a safer alternative, ensuring positive brand associations and minimizing the risks during the event."},{collapseId:21,headerTitle:"Can drone light shows be integrated with other multimedia elements?",body:"Yes, advanced drone shows can be integrated with ground-based displays, music, or other multimedia elements, providing a comprehensive sensory experience and potentially enhancing the brand's messaging through multi-sensorial engagement."},{collapseId:22,headerTitle:"How do drone light shows offer a memorable experience?",body:"A well-executed drone light show can engrave lasting memories associated with the brand into the minds of the audience, leading to positive word-of-mouth and potentially increasing brand loyalty."},{collapseId:23,headerTitle:"How flexible are drone light shows geographically?",body:"Drone shows can be executed in various locations, from urban centers to remote areas, which allows brands to target specific audiences or regions effectively, adapting the event's location to strategic marketing needs."}],d={maxWidth:"1000px",padding:"0px 0px 0px 0px",paddingBottom:o?"50px":"0px"},l=o?[...n,...r]:n;return e.jsxs(y,{id:"FAQ",children:[e.jsx(c,{}),e.jsx(f,{children:e.jsxs(m,{className:"mt-5",style:d,children:[e.jsx(u,{tag:"h1",className:"text-center mb-4 pb-2 fw-bold",style:{color:"#8806df"},children:"FAQ"}),e.jsx(v,{className:"text-center mb-5",children:"Find the answers for the most frequently asked questions below"}),e.jsx(b,{initialActive:1,style:{color:"white"},children:l.map(s=>e.jsx(w,{collapseId:s.collapseId,style:{color:"white"},headerTitle:s.headerTitle,children:e.jsx("div",{children:s.body})}))}),o?e.jsx("div",{style:{height:"100px"}}):e.jsx("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",marginTop:"20px"},children:e.jsx("button",{onClick:()=>t(!0),style:{backgroundColor:"#8806df",color:"#fff",padding:"10px 20px",border:"none",borderRadius:"5px",cursor:"pointer"},children:"View More"})})]})})]})};function j(){const[o,t]=i.useState(!0);return i.useEffect(()=>{const n=setTimeout(()=>{t(!1)},1e3);return()=>{clearTimeout(n)}},[]),e.jsxs(e.Fragment,{children:["    ",o?e.jsx(h,{}):e.jsx(x,{})]})}export{j as Page};
